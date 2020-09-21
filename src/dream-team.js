cf4a216f9ec1b275345137178f74fd3716e51d9a
AlexeyFL <alexeyfl563@gmail.com>
2020-09-21 22:29:43 +0300

    feat: resolve dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if(!Array.isArray(arr)){
+    return false;
+  }
+  const copy = [];
+	arr.forEach(item => {
+  	if(typeof item !== 'string'){
+    	return false;
+    }
+    copy.push(item.replace(/ /g, '')[0].toUpperCase());
+  })
+  
+  return copy.sort().join('');
 };
