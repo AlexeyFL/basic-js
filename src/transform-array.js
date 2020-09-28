849430c35ae5e858426b7e3aadfdb5ebe786df71
AlexeyFL <alexeyfl563@gmail.com>
2020-09-28 15:44:03 +0300

    feat: add basic sequence

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,70 +5,89 @@ module.exports = function transform(arr) {
   const discardNext = '--discard-next';
   const doubleNext = '--double-next';
   const doublePrev = '--double-prev';
+  const regx = /^--/gm;
   let copy = [];
+  let operationCount = 0;
 
+  // if not array
   if (!arr instanceof Array) {
     throw Error('THROWN');
   }
 
-  if(
-    arr.indexOf(discardPrev) === -1 ||
-    arr.indexOf(discardNext) === -1 ||
-    arr.indexOf(doubleNext) === -1 ||
-    arr.indexOf(doublePrev) === -1
-  
-  ){
+  arr.forEach(item => {
+    if(typeof item === 'string'){
+      operationCount += 1;
+    }
+  });
+
+  //if not command
+  if(!arr.some(item => regx.test(item))){
     return arr;
   }
 
+  // if not arr length
   if (!arr.length) {
     return [];
   }
-  
+
+  // if first or last position
   function checkFirstLast(item, arr){
   	if(arr.indexOf(item) === 0 || arr.indexOf(item) === arr.length -1){
     	return true;
     }
   }
 
-  if(
-    checkFirstLast(discardPrev, arr) ||
-    checkFirstLast(discardNext, arr) ||
-    checkFirstLast(doubleNext, arr) ||
-    checkFirstLast(doublePrev, arr)
-  ){
-    return arr;
-  }
+  
 
   // if discard next
   if (arr.indexOf(discardNext) !== -1) {
     let idx = arr.indexOf(discardNext);
+
+    //if first or last
     if(checkFirstLast(discardNext, arr)){
-      return;
+      copy = arr.filter((item, index, arr) => {
+        return arr.indexOf(discardNext) !== index;
+      });
+      return copy;
     }
+
     copy = arr.filter((item, index) => {
       return index !== idx && index !== idx + 1;
     });
+    return copy;
   }
-
 
   // if discard prev
   if (arr.indexOf(discardPrev) !== -1) {
+ 
     let idx = arr.indexOf(discardPrev);
-    if(checkFirstLast(discardNext, arr)){
-      return;
+
+    //if first or last
+    if(checkFirstLast(discardPrev, arr)){
+      copy = arr.filter((item, index) => {
+        return arr.indexOf(discardPrev) !== index;
+      });
+      
     }
+
     copy = arr.filter((item, index) => {
       return index !== idx && index !== idx - 1;
     });
+    return copy;
   }
   
   //if double next
   if (arr.indexOf(doubleNext) !== -1) {
-    let idx;
+    let idx = arr.indexOf(doublePrev);
+    
+    //if first or last
     if(checkFirstLast(doubleNext, arr)){
-      return;
+      copy = arr.filter((item, index) => {
+        return arr.indexOf(doubleNext) !== index;
+      });
+      return copy;
     }
+
     copy = arr.slice();
     
     if(copy.indexOf(doubleNext) !== -1){
@@ -79,14 +98,21 @@ module.exports = function transform(arr) {
     copy = copy.filter((item, index) => {
       return index !== idx;
     });
+    return copy;
   }
   
   //if double prev
   if (arr.indexOf(doublePrev) !== -1) {
-    let idx;
+    let idx = arr.indexOf(doublePrev);
+    
+    //if first or last
     if(checkFirstLast(doublePrev, arr)){
-      return;
+      copy = arr.filter((item, index) => {
+        return arr.indexOf(doublePrev) !== index;
+      });
+      return copy;
     }
+
     copy = arr.slice();
     
     if(copy.indexOf(doublePrev) !== -1){
@@ -97,5 +123,7 @@ module.exports = function transform(arr) {
     copy = copy.filter((item, index) => {
       return index !== idx;
     });
+    return copy;
   }
+  
 };
